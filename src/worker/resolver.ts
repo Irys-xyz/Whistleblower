0d7a258bef942146efe379f91d2e8fd0df3dd38c
JesseTheRobot <jesse.cruz.wright@gmail.com>
2023-07-07 12:39:23 +0000

    fix: :bug: orphan tx resolver

--- src/worker/resolver.ts
+++ src/worker/resolver.ts
@@ -26,8 +26,12 @@ export async function resolveOrphanTxs(/* orphanAgeThreshold = ORPHAN_AGE_THRESH
   await new PromisePool()
     .for(orphans)
     .withConcurrency(10)
-    .process(async (orphan) => {
-      const query = `
+    .handleError((e, i) => void logger.error(`[resolveOrphanTxs] Error while resolving orphan tx ${i.tx_id} - ${e} `))
+    .process(resolveOrphanTx);
+}
+
+export async function resolveOrphanTx(orphan: Pick<Transactions, "tx_id">): Promise<void> {
+  const query = `
           query {
           transaction(id: "${orphan.tx_id}"){
             block {
@@ -38,19 +42,19 @@ export async function resolveOrphanTxs(/* orphanAgeThreshold = ORPHAN_AGE_THRESH
             }
           }
         }`;
-      const res = await retryRequest<{
-        data: { transaction: { block: { height: number } | null; bundledIn: { id: string } | null } };
-      }>(GATEWAY_URL + "/graphql", {
-        data: { query },
-        method: "post",
-        headers: { "content-type": "application/json" },
-      });
-      const blockInfo = res.data.data.transaction.block;
-      const bundledInId = res.data.data.transaction.bundledIn?.id;
-      if (!blockInfo) return void logger.verbose(`[resolveOrphanTx] Block is null for ${orphan.tx_id} - delaying`);
-      if (bundledInId) {
-        // get owner of the bundle from L1
-        const ownerQuery = `
+  const res = await retryRequest<{
+    data: { transaction: { block: { height: number } | null; bundledIn: { id: string } | null } };
+  }>(new URL("/graphql", GATEWAY_URL), {
+    data: { query },
+    method: "post",
+    headers: { "content-type": "application/json" },
+  });
+  const blockInfo = res.data.data.transaction.block;
+  const bundledInId = res.data.data.transaction.bundledIn?.id;
+  if (!blockInfo) return void logger.verbose(`[resolveOrphanTx] Block is null for ${orphan.tx_id} - delaying`);
+  if (bundledInId) {
+    // get owner of the bundle from L1
+    const ownerQuery = `
         query {
           transaction(id: "${bundledInId}") {
             owner {
@@ -58,49 +62,47 @@ export async function resolveOrphanTxs(/* orphanAgeThreshold = ORPHAN_AGE_THRESH
             }
           }
         }`;
-        const ownerRes = await retryRequest<{
-          data: { transaction: { owner: { address?: string } | null } };
-        }>(GATEWAY_URL + "/graphql", {
-          data: { query: ownerQuery },
-          method: "post",
-          headers: { "content-type": "application/json" },
-        });
-
-        const ownerAddress = ownerRes.data.data.transaction.owner?.address;
-        if (!ownerAddress)
-          return void logger.error(
-            `[resolveOrphanTx] Unable to determine owner for L1 bundle ${bundledInId} for orphan tx ${orphan.tx_id}`,
-          );
-
-        const ownerUrl = await database<Bundlers>("bundlers")
-          .select("url")
-          .where("address", "=", ownerAddress)
-          .first()
-          .then((v) => v?.url);
-
-        if (!ownerUrl)
-          return void logger.error(
-            `[resolveOrphanTx] Unable to determine owner(${ownerAddress}) URL for L1 bundle ${bundledInId} for orphan tx ${orphan.tx_id}`,
-          );
-        logger.warn(`[resolveOrphanTx] Unexpected orphan Tx ${orphan.tx_id} from ${ownerUrl}`);
-
-        await database<Bundles>("bundles")
-          .insert({
-            tx_id: bundledInId,
-            block: blockInfo.height,
-            from_node: ownerUrl.toString(),
-            date_created: new Date(),
-          })
-          .onConflict("tx_id")
-          .ignore();
-
-        await database<Transactions>("transactions")
-          .update("bundled_in", bundledInId)
-          .where("tx_id", "=", orphan.tx_id);
-        return;
-      }
-      return;
+    const ownerRes = await retryRequest<{
+      data: { transaction: { owner: { address?: string } | null } };
+    }>(new URL("/graphql", GATEWAY_URL), {
+      data: { query: ownerQuery },
+      method: "post",
+      headers: { "content-type": "application/json" },
     });
+
+    const ownerAddress = ownerRes.data.data.transaction.owner?.address;
+    if (!ownerAddress)
+      return void logger.error(
+        `[resolveOrphanTx] Unable to determine owner for L1 bundle ${bundledInId} for orphan tx ${orphan.tx_id}`,
+      );
+
+    const ownerUrl = await database<Bundlers>("bundlers")
+      .select("url")
+      .where("address", "=", ownerAddress)
+      .first()
+      .then((v) => v?.url);
+
+    if (!ownerUrl)
+      return void logger.error(
+        `[resolveOrphanTx] Unable to determine owner(${ownerAddress}) URL for L1 bundle ${bundledInId} for orphan tx ${orphan.tx_id}`,
+      );
+    logger.warn(`[resolveOrphanTx] Unexpected orphan Tx ${orphan.tx_id} from ${ownerUrl}`);
+
+    await database<Bundles>("bundles")
+      .insert({
+        tx_id: bundledInId,
+        block: blockInfo.height,
+        from_node: ownerUrl.toString(),
+        date_created: new Date(),
+        is_valid: undefined,
+      })
+      .onConflict("tx_id")
+      .ignore();
+
+    await database<Transactions>("transactions").update("bundled_in", bundledInId).where("tx_id", "=", orphan.tx_id);
+    return;
+  }
+  return;
 }
 
 export async function getBlock(blockId: string | number): Promise<BlockData> {
