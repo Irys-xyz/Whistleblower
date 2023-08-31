403c5bac9788f96b6a0641cb0b09ffdfcd68dd6c
JesseTheRobot <jesse.cruz.wright@gmail.com>
2023-08-31 19:16:54 +0000

    fix: :bug: utils/arweave: Improve getNetworkHeight function

--- src/utils/arweave.ts
+++ src/utils/arweave.ts
@@ -5,15 +5,17 @@ import AsyncRetry from "async-retry";
 
 export const arweave = Arweave.init(GATEWAY_CONFIG);
 
-let cachedHeight: Promise<number> | undefined = undefined;
+let cachedHeight: Promise<number | undefined> | undefined = undefined;
 
 export async function getNetworkHeight(cacheLifeMs = 10_000): Promise<number> {
-  if (cachedHeight) return cachedHeight;
+  const cv = await cachedHeight;
+  if (cv) return cv;
   logger.debug(`[getNetworkHeight] refreshing arweave network info`);
   // set cachedHeight as a promise so we don't try to get height multiple times when it expires
   cachedHeight = AsyncRetry(async (_) => {
     return await arweave.network.getInfo().then((info) => info.height);
-  });
+  }).catch((e) => void logger.warn(`[getNetworkHeight] Error updating network height - ${e}`));
+
   setTimeout(() => (cachedHeight = undefined), cacheLifeMs);
-  return cachedHeight;
+  return getNetworkHeight();
 }
